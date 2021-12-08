1be0113b8e24c7f189eb28fc91a639025070ced1
araguaci <araguaci@gmail.com>
2021-12-07 22:11:38 -0300

    share url link

--- .history/js/share_20211207215727.js
+++ .history/js/share_20211207215727.js
@@ -5,14 +5,16 @@ shareBtn.addEventListener('click', () => {
     shareOptions.classList.toggle('active');
 })
 
-function clipboard(id) {
+function clipboard() {
     /* Get the text field */
-    var r = document.createRange();
-    r.selectNode(document.getElementById(id));
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(r);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
+    var copyText = document.getElementById("urllink");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
   
     /* Alert the copied text */
     shareOptions.classList.toggle('active');
